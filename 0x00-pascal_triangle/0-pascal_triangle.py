766fd5a07f6bf5109a306e7130890d4f6c232088
dominic-source <chinonsodomnic@gmail.com>
2024-01-04 10:38:39 +0100

    implement pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+a module that implement the pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """The pascal triangle implementation"""
+
+    triangle = [[1],]
+    for id in range(n - 1):
+        dat = triangle[-1]
+        lit_sqr = [1]
+        for idx, current in enumerate(dat):
+            try:
+                next = dat[idx + 1]
+                lit_sqr.append(current + next)
+            except Exception:
+                lit_sqr.append(1)
+        triangle.append(lit_sqr)
+    return triangle
