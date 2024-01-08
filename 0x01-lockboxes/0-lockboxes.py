6316cf7ddf7a7ad4881814713381de0a80427b41
dominic-source <chinonsodomnic@gmail.com>
2024-01-08 15:50:24 -0800

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Module to practice python dsa using lockboxes algorithm"""
+
+
+def canUnlockAll(boxes):
+    """find boxes that cannot be unlocked"""
+
+    unlockedBoxes = 0
+    setOfKeys = set([0])
+    lockedBoxes = set()
+
+    for idx, box in enumerate(boxes):
+        if idx not in setOfKeys:
+            lockedBoxes.add(idx)
+        elif idx in setOfKeys:
+            unlockedBoxes += 1
+            setOfKeys.update(box)
+            for locked in lockedBoxes:
+                if locked in setOfKeys:
+                    unlockedBoxes += 1
+                    setOfKeys.add(locked)
+    if unlockedBoxes < len(boxes):
+        return False
+    return True
