3adc21cd29134a0043d003970fbbb440d1aa5684
dominic-source <chinonsodomnic@gmail.com>
2024-01-30 10:12:11 +0100

    Debug v4: validate utf8 data

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,8 +7,16 @@
 
 def validUTF8(data):
     """Detect a valid utf-8 encoding"""
-
-    for i in data:
-        if not (i <= 0b01111111):
-            return False
+    byte = data[0]
+    data_s = data[1:]
+    if byte <= 0b01111111:
+        # single byte
+        for i in data_s:
+            if not (i <= 0b01111111):
+                return False
+    elif byte >= 0b11000000:
+        # two bytes
+        for i in data_s:
+            if (i & 0b1000000) == 0:
+                return False
     return True
