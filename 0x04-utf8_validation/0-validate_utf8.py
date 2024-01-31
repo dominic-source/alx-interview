090491403a60276822b29f65174b3656cdf9ad93
dominic-source <chinonsodomnic@gmail.com>
2024-01-31 21:03:05 +0100

    Solved: validate utf data set

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,16 +7,24 @@
 
 def validUTF8(data):
     """Detect a valid utf-8 encoding"""
-    byte = data[0]
-    data_s = data[1:]
-    if byte <= 0b01111111:
-        # single byte
-        for i in data_s:
-            if not (i <= 0b01111111):
-                return False
-    elif byte >= 0b11000000:
-        # two bytes
-        for i in data_s:
-            if (i & 0b1000000) == 0:
-                return False
+    count = 0
+    for i in data:
+
+        # Check for continuation byte
+        if (i & 0b10000000) != 0 and (i & 0b01000000) == 0 and count > 0:
+            count -= 1
+            continue
+        elif (i & 0b10000000) != 0 and (i & 0b01000000) == 0 and count <= 0:
+            return False
+        elif (i & 0b10000000) == 0 and count == 0:
+            continue
+        elif (i & 0b10000000) == 0 and count > 0:
+            return False
+
+        # count the number of initial 1 bit bits
+        start_bin = 0b10000000
+        while (i & start_bin) and count < 5:
+            count += 1
+            start_bin >>= 1
+
     return True
