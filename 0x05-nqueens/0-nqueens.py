f35e5be692cb1d3449c6b55a8f821991fec4c45f
dominic-source <chinonsodomnic@gmail.com>
2024-02-09 22:04:59 +0100

    solved: display solutions to N queen problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,10 +3,9 @@
 """Learn about the n queens problem and how to solve it"""
 from typing import List
 import sys
-import time
 
 args = sys.argv
-if len(args) > 2:
+if len(args) != 2:
     print("Usage: nqueens N")
     exit(1)
 
@@ -40,93 +39,56 @@ def valid_column(index: int, matrix: List[List[int]]) -> bool:
 def valid_diagonal(matrix: List[List[int]], row: int, column: int) -> bool:
     """Validate a diagonal that a queen can be placed"""
 
-    if row >= N or column >= N or row < 0 or column < 0:
-        return False
-
-    r = row + 1
-    c = column + 1
-    while c >= 0 and r >= 0 and r < N and c < N:
-        if matrix[r][c]:
+    for i in range(N):
+        if (matrix[row][i] == 1) or (matrix[i][column] == 1):
             return False
-        c += 1
-        r += 1
 
-    r = row - 1
-    c = column - 1
-    while c >= 0 and r >= 0 and r < N and c < N:
-        if matrix[r][c]:
-            return False
-        c -= 1
-        r -= 1
-    
-    r = row - 1
-    c = column + 1
-    while c >= 0 and r >= 0 and r < N and c < N:
-        if matrix[r][c]:
-            return False
-        c += 1
-        r -= 1
-
-    r = row + 1
-    c = column - 1
-    while c >= 0 and r >= 0 and r < N and c < N:
-        if matrix[r][c]:
-            return False
-        c -= 1
-        r += 1
-
+    for i in range(N):
+        for j in range(N):
+            if (i + j == row + column) or (i - j == row - column):
+                if matrix[i][j] == 1:
+                    return False
     return True
 
 
-def reset_row(row_ind: int, matrix: List[List[int]]) -> None:
-    """This will reset a row to be zero"""
-    if row_ind > -1 and matrix:
-        i = 0
-        while i < N:
-            matrix[row_ind][i] = 0
-            i += 1
-
-def get_index(row: int, matrix: List[List[int]]) -> int:
+def get_index(arr: List[int]) -> int:
     """Get index of position 1"""
     i = 0
     while i < N:
-        if matrix[row][i] == 1:
+        if arr[i] == 1:
             return i
         i += 1
     return -1
 
 
-def print_matrix():
-    for row in matrix:
-        print(row)
-    print("")
-    time.sleep(1)
+def print_matrix(matrix: List[List[int]]):
+    """Print the positions of the n queen solution"""
+    index = []
+    for idx, row in enumerate(matrix):
+        index.append([idx, get_index(row)])
+    print(index)
 
 
-def generate_solutions():
+def generate_solutions(col: int, matrix: List[List[int]],
+                       all_solutions: List[List[List[int]]]) -> bool:
     """Generate solutions for the N queens problem"""
-    col, row  = 0, 0
-    solution = False
-    while not solution:
-        if valid_column(col, matrix) and valid_row(matrix[row]) and valid_diagonal(matrix, row, col):
-            matrix[row][col] = 1
-            row += 1
-            print_matrix()
-        else:
-            print_matrix()
-            col += 1
-            if col == N:
-                reset_row(row, matrix)
-                row -= 1
-                nex = get_index(row, matrix)
-                if nex != -1:
-                    col = nex + 1
-                else:
-                    col = 0
-        if row == N:
-            solution = True
-    print(matrix)
+    if col >= N:
+        all_solutions.append([row[:] for row in matrix])
+        return
+
+    for i in range(N):
+        if valid_column(col, matrix) and valid_row(
+                matrix[i]) and valid_diagonal(matrix, i, col):
+
+            matrix[i][col] = 1
+
+            generate_solutions(col + 1, matrix, all_solutions)
+            matrix[i][col] = 0
 
 
 if __name__ == '__main__':
-    generate_solutions()
+    all_solutions = []
+    generate_solutions(0, matrix, all_solutions)
+
+    for solution in all_solutions:
+        print_matrix(solution)
