99e12ff7c31d9e3206d7d08df7e5bae187775c55
dominic-source <chinonsodomnic@gmail.com>
2024-02-09 13:03:36 +0000

    in Progress: find n queen solutions

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Learn about the n queens problem and how to solve it"""
-
+from typing import List
 import sys
 
 args = sys.argv
@@ -19,3 +19,85 @@ except ValueError:
 if N < 4:
     print("N must be at least 4")
     exit(1)
+
+matrix = [[0] * N for _ in range(N)]
+
+
+def valid_row(row: List[int]) -> bool:
+    """Validate a row that a queen can be placed"""
+    return sum(row) == 0
+
+
+def valid_column(index: int, matrix: List[List[int]]) -> bool:
+    """Validate a column that a queen can be placed"""
+    for row in matrix:
+        if row[index] == 1:
+            return False
+    return True
+
+
+def valid_diagonal(matrix: List[List[int]], row: int, column: int) -> bool:
+    """Validate a diagonal that a queen can be placed"""
+
+    if row >= N or column >= N or row < 0 or column < 0:
+        return False
+
+    r = row + 1
+    c = column + 1
+    while c >= 0 and r >= 0 and r < N and c < N:
+        if matrix[r][c]:
+            return False
+        c += 1
+        r += 1
+
+    r = row - 1
+    c = column - 1
+    while c >= 0 and r >= 0 and r < N and c < N:
+        if matrix[r][c]:
+            return False
+        c -= 1
+        r -= 1
+    
+    r = row - 1
+    c = column + 1
+    while c >= 0 and r >= 0 and r < N and c < N:
+        if matrix[r][c]:
+            return False
+        c += 1
+        r -= 1
+
+    r = row + 1
+    c = column - 1
+    while c >= 0 and r >= 0 and r < N and c < N:
+        if matrix[r][c]:
+            return False
+        c -= 1
+        r += 1
+
+    return True
+
+
+def reset_row(row_ind: int, matrix: List[List[int]]) -> None:
+    """This will reset a row to be zero"""
+    if row_ind > -1 and matrix:
+        i = 0
+        while i < N:
+            matrix[row_ind][i] = 0
+            i += 1
+
+
+def generate_solutions():
+    """Generate solutions for the N queens problem"""
+    col, row  = 0, 0
+    solution = False
+    while not solution:
+        if valid_column(col, matrix) and valid_row(matrix[row]) and valid_diagonal(matrix, row, column):
+            matrix[row][col] = 1
+            row += 1
+        else:
+            col += 1
+    print(matrix)
+
+
+if __name__ == '__main__':
+    generate_solutions()
