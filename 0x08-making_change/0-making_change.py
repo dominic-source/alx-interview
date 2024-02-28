219123e96ad05c9fc888e1d2be0be6d0c97fb6a9
dominic-source <chinonsodomnic@gmail.com>
2024-02-28 20:19:02 +0100

    making changes of coins solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This module manages Changing coins"""
+
+
+def makeChange(coins, total):
+    """get the count of minimum count of coins change"""
+    if total <= 0:
+        return 0
+    sorted_coins = sorted(coins)
+    length = len(sorted_coins) - 1
+    r_amount = total
+    ans = []
+    i = 0
+    while i < length and r_amount >= 0:
+        if r_amount >= sorted_coins[~i]:
+            ans.append(sorted_coins[~i])
+            r_amount -= sorted_coins[~i]
+        else:
+            i += 1
+
+    if r_amount > 0:
+        return -1
+    else:
+        return len(ans)
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+# Implement a greedy algorithm to find the minimum number of coins to make a certain total
